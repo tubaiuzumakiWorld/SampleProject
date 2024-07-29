0997c3e10c09dbac8b59ad7a7f8e3de24b303e2a
myboss <subhodeepmukherjee72@gmail.com>
2024-07-29 21:56:50 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className='w-full back h-screen flex flex-col relative overflow-x-hidden items-center '>
-       <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px]  px-10 py-2 text-4xl font-bold '>RANDOM GIF</h1>
+       <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px]  px-10 py-2 text-4xl font-bold '>RANDOM Gf</h1>
        <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
           <Random />
           <Tag />
